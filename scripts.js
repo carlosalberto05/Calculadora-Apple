f401bca632ce0667751203d648424d60744e9f1f
Carlos Alberto Lira <47789867+carlosalberto05@users.noreply.github.com>
2020-01-28 11:47:32 -0600

    Se creo la funcion para hacer el calculo de la suma

--- scripts.js
+++ scripts.js
@@ -4,43 +4,45 @@ var inputValueMemo = 0;
 //Primero vamos a leer lo que el usuario presione
 //cuando le den click a algun boton
 function getContentClick(event) {
-  const value = event.target.innerHTML;
-  filterAction(value);
+    const value = event.target.innerHTML;
+    filterAction(value);
 }
 
 //Luego vamos a filtar para que ver si es un numero o alguna operacion
 const filterAction = value => {
-  value === "0" ? addNumberInput(0) : null;
-  value === "1" ? addNumberInput(1) : null;
-  value === "2" ? addNumberInput(2) : null;
-  value === "3" ? addNumberInput(3) : null;
-  value === "4" ? addNumberInput(4) : null;
-  value === "5" ? addNumberInput(5) : null;
-  value === "6" ? addNumberInput(6) : null;
-  value === "7" ? addNumberInput(7) : null;
-  value === "8" ? addNumberInput(8) : null;
-  value === "9" ? addNumberInput(9) : null;
-  value === "," ? addNumberInput(',') : null;
+    value === "0" ? addNumberInput(0) : null;
+    value === "1" ? addNumberInput(1) : null;
+    value === "2" ? addNumberInput(2) : null;
+    value === "3" ? addNumberInput(3) : null;
+    value === "4" ? addNumberInput(4) : null;
+    value === "5" ? addNumberInput(5) : null;
+    value === "6" ? addNumberInput(6) : null;
+    value === "7" ? addNumberInput(7) : null;
+    value === "8" ? addNumberInput(8) : null;
+    value === "9" ? addNumberInput(9) : null;
+    value === "," ? addNumberInput(',') : null;
 
-  value === "+" ? setOperation('+') : null;
-  value === "-" ? setOperation('-') : null;
-  value === "X" ? setOperation('*') : null;
-  value === "/" ? setOperation('/') : null;
-  value === "%" ? setOperation('%') : null;
-  value === "+/-" ? setOperation('+/-') : null;
+    value === "+" ? setOperation('+') : null;
+    value === "-" ? setOperation('-') : null;
+    value === "X" ? setOperation('*') : null;
+    value === "/" ? setOperation('/') : null;
+    value === "%" ? setOperation('%') : null;
+    value === "+/-" ? setOperation('+/-') : null;
+
+    value === "=" ? calculation() : null;
 };
 
 //Vamos a cambiar el valor de la screen al presionar algun número
-function addNumberInput(value){
-   const inputScreen =  document.getElementsByClassName('calculator__screen')[0];
-   const inputValue = inputScreen.value; 
+function addNumberInput(value) {
+    const inputScreen = document.getElementsByClassName('calculator__screen')[0];
+    const inputValue = inputScreen.value;
 
-   if(inputValue === "0" && inputValue.length === 1 && value !== ",") {
-       inputScreen.value = value;
-       return;
-   }
+    if (inputValue === "0" && inputValue.length === 1 && value !== ",") {
+        inputScreen.value = value;
+        return;
+    }
 
-   inputScreen.value = inputValue +  value;
+    inputScreen.value = inputValue + value;
 }
 
 //Funcion para guardar la operacion seleccionada y calcular la suma
@@ -48,22 +50,38 @@ function setOperation(operator) {
     const inputScreenValue = document.getElementsByClassName('calculator__screen')[0].value;
     this.operator = operator;
 
-    if(inputScreenValue != 0) {
+    if (inputScreenValue != 0) {
         calculation();
     }
 }
 
-function calculation(){
+function calculation() {
     const inputScreen = document.getElementsByClassName('calculator__screen')[0];
-    let valueOne = this.inputValueMemo;
-    let valueTwo = inputScreen.value;
+    let valueOne = transformCommaToPoint(this.inputValueMemo);
+    let valueTwo = transformCommaToPoint(inputScreen.value);
     let total = 0;
 
-    if(this.operator === "+" && inputScreen !== ""){
+    //Operacion de suma
+    if (this.operator === "+" && inputScreen !== "") {
         total = valueOne + valueTwo;
     }
 
+    total = transformPointToComma(total);
     this.inputValueMemo = total;
     inputScreen.value = "";
     inputScreen.placeholder = total;
+}
+
+function transformCommaToPoint(value) {
+    if (typeof value !== "number") {
+        let resultTransform = value.replace(',', '.');
+        return parseFloat(resultTransform);
+    }
+    return value;
+}
+
+function transformPointToComma(value) {
+    let resultTransform = value.toString();
+    resultTransform = resultTransform.replace('.', ',');
+    return resultTransform;
 }
